207a6798f9266e25eb7742d055640155b3ee9cba
NUC-2004 <728697938@qq.com>
2025-09-19 20:31:15 +1000

    35complete

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip introMusic;
+    public AudioClip normalMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = introMusic;
+        audioSource.Play();
+        Invoke("PlayNormalMusic", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void PlayNormalMusic()
+    {
+        audioSource.clip = normalMusic;
+        audioSource.loop = true; 
+        audioSource.Play();
+    }
+}
